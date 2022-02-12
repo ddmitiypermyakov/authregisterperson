7877a7074469426a86e450e3289091e73246fbfb
dmitry <ddmitiy@mail.ru>
2022-02-12 17:17:26 +0300

    Validation login exists database

--- register.php
+++ register.php
@@ -1,5 +1,5 @@
 <table>
-<form method=post>
+<form action = "" method=post>
 <tr><td>Имя:</td><td><input type=text name=name required minlength="2" maxlength="2"></td></tr>
 <tr><td>Логин:</td><td><input type=text name=login required minlength="6"></td></tr>
 <tr><td>Пароль:</td><td><input type=password name=password required minlength="6"></td></tr>
@@ -23,14 +23,17 @@
 
     include "validation_person.php";
     //задаем конструктор
-    $value_user = new ValidationAuth($_POST['name'],$_POST['login'],$_POST['password'],$_POST['confirm_password'], $_POST['email']);
+    $value_user = new ValidationAuth(trim($_POST['name']),trim($_POST['login']),trim($_POST['password']),trim($_POST['confirm_password']), trim($_POST['email']));
     //Подключаем валидацию
-
+    $filename = "bd.json";
+    $arr = file($filename);
     $value_user->alnumPassword();
     $value_user->boolPasswordandConfirmPassword();
     $value_user->validationEmail();
     $value_user->validationAlName();
 
+    $a = $value_user->tempArray($arr);
+    $value_user->personValidationExists($a);
     include "crud_bd.php";
 
 
@@ -39,5 +42,5 @@
     $save_database->AddDatabaseJSON();
   };
   //очищаем сессию
-  session_unset();
+  //session_unset();
   ?>
