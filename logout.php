4287d85b88b341c0e987885795ed0cc5bddde293
dmitry <ddmitiy@mail.ru>
2022-02-14 10:08:07 +0300

    Correct files

--- logout.php
+++ logout.php
@@ -4,6 +4,6 @@ session_start();
 setcookie(session_name(), "", time() - 3600); //отправляет браузеру удалить текущую session_name (session_id) из кук
 session_destroy(); //удалить сохраненный session_login
 session_write_close();
-header('Location: /testask/testask/');
+header('Location: /testask/');
 
 ?>
